a8692ed674fe3a084ccd7d7fcd4d1114af22cf92
Oliver Meyn <oliver@mineallmeyn.com>
2014-05-21 10:59:39 +0200

    mavenize backfill cluster configs and update backfill binaries to use profiles and proper configs

--- cube/bin/occurrence/backfill.sh
+++ cube/bin/occurrence/backfill.sh
@@ -1,2 +1,2 @@
-mvn -f ../../pom.xml clean assembly:assembly
-java -cp ./:../conf:../../target/cube-0.13-SNAPSHOT-jar-with-dependencies.jar:$HADOOP_HOME/lib/hadoop-hdfs-2.0.0-cdh4.2.1.jar org.gbif.metrics.cube.occurrence.backfill.Backfill
+mvn -f ../../pom.xml -Puat clean assembly:assembly
+java -cp ./:../../target/classes:../../target/cube-0.14-SNAPSHOT-jar-with-dependencies.jar:$HADOOP_HOME/lib/hadoop-hdfs-2.0.0-cdh4.2.1.jar org.gbif.metrics.cube.occurrence.backfill.Backfill
