ca05c1fb6b5ab651d7720ba47ef5dfaca963bada
Oliver Meyn <oliver@mineallmeyn.com>
2014-07-22 15:00:16 +0200

    update cube building scripts to latest snapshot

--- cube/bin/country/occurrence-dataset/no-build-backfill.sh
+++ cube/bin/country/occurrence-dataset/no-build-backfill.sh
@@ -1,1 +1,1 @@
-java -cp .:../../../target/classes:../../../target/cube-0.14-SNAPSHOT-jar-with-dependencies.jar:$HADOOP_HOME/lib/hadoop-hdfs-2.0.0-cdh4.2.1.jar org.gbif.metrics.cube.index.country.backfill.Backfill
+java -cp .:../../../target/classes:../../../target/cube-0.17-SNAPSHOT-jar-with-dependencies.jar:$HADOOP_HOME/lib/hadoop-hdfs-2.0.0-cdh4.2.1.jar org.gbif.metrics.cube.index.country.backfill.Backfill
