6ed2c8894679bf9000b8672871dd523dbbbf2531
Federico Mendez <fmendez@gbif.org>
2014-10-08 16:18:28 +0200

    metrics-ws modified to use the project gbif-microservice

--- metrics-ws/src/main/java/org/gbif/metrics/ws/guice/MetricsWsServletListener.java
+++ metrics-ws/src/main/java/org/gbif/metrics/ws/guice/MetricsWsServletListener.java
@@ -3,12 +3,16 @@ package org.gbif.metrics.ws.guice;
 import org.gbif.metrics.cube.index.country.guice.CountryOccurrenceDatasetHBaseModule;
 import org.gbif.metrics.cube.index.taxon.guice.TaxonOccurrenceDatasetHBaseModule;
 import org.gbif.metrics.cube.occurrence.guice.OccurrenceCubeHBaseModule;
+import org.gbif.utils.file.properties.PropertiesUtil;
+import org.gbif.ws.app.ConfUtils;
 import org.gbif.ws.server.guice.GbifServletListener;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
 import com.google.common.collect.Lists;
+import com.google.common.io.Resources;
 import com.google.inject.Module;
 
 
@@ -17,11 +21,10 @@ import com.google.inject.Module;
  */
 public class MetricsWsServletListener extends GbifServletListener {
 
-  public static final String APPLICATION_PROPERTIES = "metrics.properties";
+  private static final String METRICS_CONF = "metrics.properties";
 
-  public MetricsWsServletListener() {
-    super(APPLICATION_PROPERTIES, "org.gbif.metrics.ws", false);
-
+  public MetricsWsServletListener() throws IOException {
+    super(PropertiesUtil.readFromFile(ConfUtils.getAppConfFile(METRICS_CONF)), "org.gbif.metrics.ws", false);
   }
 
   @Override
