b7b665736d34d16d2653ac0556aa656434687810
fmendezh <federicomh@gmail.com>
2024-06-04 15:26:26 -0500

    authorizeRequests

--- metrics-ws/src/main/java/org/gbif/metrics/ws/config/WebSecurityConfigurer.java
+++ metrics-ws/src/main/java/org/gbif/metrics/ws/config/WebSecurityConfigurer.java
@@ -29,7 +29,7 @@ public class WebSecurityConfigurer {
 
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(
+    http.authorizeRequests(
         authorizeRequests ->
           authorizeRequests.anyRequest().permitAll())
       .httpBasic(AbstractHttpConfigurer::disable)
