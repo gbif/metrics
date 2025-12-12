ae7b11156db31b675d9a712edb0f1ed3262784a9
fmendezh <federicomh@gmail.com>
2025-12-12 14:55:12 +0100

    upgrading dependencies to spring 3.5.8

--- metrics-ws/src/main/java/org/gbif/metrics/ws/config/WebSecurityConfigurer.java
+++ metrics-ws/src/main/java/org/gbif/metrics/ws/config/WebSecurityConfigurer.java
@@ -20,20 +20,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfigurer {
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.httpBasic().disable().csrf().disable().cors();
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.httpBasic(AbstractHttpConfigurer::disable)
+      .csrf(AbstractHttpConfigurer::disable)
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+      .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+    return http.build();
   }
 
   @Bean
@@ -43,12 +49,12 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type"));
     configuration.setAllowedOrigins(Collections.singletonList("*"));
     configuration.setAllowedMethods(
-        Arrays.asList("HEAD", "GET", "POST", "DELETE", "PUT", "OPTIONS"));
+      Arrays.asList("HEAD", "GET", "POST", "DELETE", "PUT", "OPTIONS"));
     configuration.setExposedHeaders(
-        Arrays.asList(
-            "Access-Control-Allow-Origin",
-            "Access-Control-Allow-Methods",
-            "Access-Control-Allow-Headers"));
+      Arrays.asList(
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods",
+        "Access-Control-Allow-Headers"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
